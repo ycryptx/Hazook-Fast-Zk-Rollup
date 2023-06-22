a53123486ffead438107bc3c06deb0aac5233904
ycryptx <yonatan.med@gmail.com>
2023-06-22 14:47:26 +0900

    sequencer uploader

--- sequencer/src/index.ts
+++ sequencer/src/index.ts
@@ -1,13 +1,19 @@
 import { EMRClient, AddInstanceFleetCommand, AddInstanceFleetCommandInput } from "@aws-sdk/client-emr"
+import { Uploader, Mode } from "./uploader";
 
 const main = async () => {
   const args = process.argv.slice(2);
+  const [region, _] = args
 
   if (args.length < 2) {
     throw new Error("Missing args")
   }
 
-  const client = new EMRClient({ region: args[0] })
+  const emrCLient = new EMRClient({ region })
+  const dataUploader = new Uploader(Mode.LOCAL, region)
+
+  // uplaod data to Hadoop
+  await dataUploader.upload({})
 
   // TODO: call EMR with the job
 
@@ -18,7 +24,7 @@ const main = async () => {
   const command = new AddInstanceFleetCommand(params)
 
   try {
-    const data = await client.send(command)
+    const data = await emrCLient.send(command)
     console.log(data)
     // process data.
   } catch (error) {
---
+++ sequencer/src/uploader/uploader.ts
@@ -0,0 +1,38 @@
+import { S3Client } from "@aws-sdk/client-s3"
+import { Mode } from "./types";
+
+
+/**
+ * Uploads data to make it available to the Hadoop map-reduce pipeline
+ */
+export class Uploader {
+    private mode: Mode
+    private s3Client?: S3Client
+
+
+    constructor(mode: Mode, region: string) {
+        this.mode = mode
+        if (this.mode == Mode.EMR) {
+            this.s3Client = new S3Client({ region })
+        } else if (this.mode == Mode.LOCAL) {
+            // do something
+        }
+    }
+
+    public async upload(data: any): Promise<void> {
+        return this.mode == Mode.LOCAL ? this._uploadToLocalHadoop(data) : this._uploadToS3(data)
+    }
+
+    private async _uploadToLocalHadoop(data: any): Promise<void> {
+        data
+        return
+    }
+
+    // https://docs.aws.amazon.com/AmazonS3/latest/userguide/optimizing-performance-guidelines.html
+    private async _uploadToS3(data: any): Promise<void> {
+        // probably a good idea to use Multipart Upload
+        this.s3Client
+        data
+        return
+    }
+}
