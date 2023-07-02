fc92fb69440502da5dae1687b7c3708e52f926ed
ycryptx <yonatan.med@gmail.com>
2023-07-02 15:56:41 +0300

    fix tests

--- sequencer/__tests__/integration.test.ts
+++ sequencer/__tests__/integration.test.ts
@@ -1,4 +1,4 @@
-import 'dotenv';
+import 'dotenv/config';
 import { resolve } from 'path';
 import { MapReduceClient } from '../src/map-reduce';
 import { Mode } from '../src/map-reduce';
@@ -12,29 +12,23 @@ describe('integration tests', () => {
   it('1. demo-0: should sum numbers correctly', async () => {
     const dataFilePath = resolve(__dirname, '../..', 'data/demo-0/run1.txt');
     const inputLocation = await mapReduce.upload(dataFilePath);
-    expect((await mapReduce.process(inputLocation)).trim()).toEqual(`${2 * 8}`);
+    expect(await mapReduce.process(inputLocation)).toEqual(`${2 * 8}`);
   });
   it('2. demo-0: should sum numbers correctly', async () => {
     const dataFilePath = resolve(__dirname, '../..', 'data/demo-0/run2.txt');
     const inputLocation = await mapReduce.upload(dataFilePath);
-    expect((await mapReduce.process(inputLocation)).trim()).toEqual(
-      `${3 * 64}`,
-    );
+    expect(await mapReduce.process(inputLocation)).toEqual(`${3 * 64}`);
   });
 
   it('3. demo-0: should sum numbers correctly', async () => {
     const dataFilePath = resolve(__dirname, '../..', 'data/demo-0/run3.txt');
     const inputLocation = await mapReduce.upload(dataFilePath);
-    expect((await mapReduce.process(inputLocation)).trim()).toEqual(
-      `${4 * 256}`,
-    );
+    expect(await mapReduce.process(inputLocation)).toEqual(`${4 * 256}`);
   });
 
   it('4. demo-0: should sum numbers correctly', async () => {
     const dataFilePath = resolve(__dirname, '../..', 'data/demo-0/run4.txt');
     const inputLocation = await mapReduce.upload(dataFilePath);
-    expect((await mapReduce.process(inputLocation)).trim()).toEqual(
-      `${5 * 2048}`,
-    );
+    expect(await mapReduce.process(inputLocation)).toEqual(`${5 * 2048}`);
   });
 });
