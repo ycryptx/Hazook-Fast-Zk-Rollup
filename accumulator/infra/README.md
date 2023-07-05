535cfb9cf309cab3815c8823ddfd6233d298348c
Ninjatrappeur <NinjaTrappeur@users.noreply.github.com>
2023-07-05 10:05:05 +0200

    Infra: add Sequencer (#8)
    * Infra: add Sequencer
    
    We create a EC2 machine running the fast-zk-rollup docker container.
    
    The NixOS EC2 machine loads this container through a systemd-managed
    podman runtime. The EC2 machine also runs a Nginx reverse proxy
    listening on 80. We'll add a TLS layer once we'll have a proper
    route53 setup for the domain name.
    
    We attach a policy to the EC2 machine allowing the services running on
    this machine to retrieve a temporary access token to control the EMR
    setup (ie. adding tasks, etc.).
    
    * github action: run on main
    
    We were only running the github action on the PRs, preventing us to
    run the deployment job when a commit reaches main.
    
    * use the correct sequencer Dockerfile in CI
    
    ---------
    
    Co-authored-by: ycryptx <yonatan.med@gmail.com>

--- accumulator/infra/README.md
+++ accumulator/infra/README.md
@@ -12,3 +12,7 @@ aws emr create-default-roles
 ```
 
 This will create the default AWS EMR roles. This deployment depends upon them.
+
+# Sequencer NixOS Configuration
+
+The sequencer is launched on a NixOS EC2 machine. This machine runs the Sequencer docker image through a systemd-managed podman service. This service is reverse-proxied by Nginx.
--- accumulator/infra/main.tf
+++ accumulator/infra/main.tf
@@ -128,7 +128,7 @@ resource "aws_s3_object" "emr_mapper" {
 # subnet. The routing table is pretty straightforward: a simple
 # non-NATed gateway.
 
-resource "aws_subnet" "public-1" {
+resource "aws_subnet" "public_1" {
   vpc_id = "${aws_vpc.main.id}"
   cidr_block = "10.0.0.0/24"
   map_public_ip_on_launch = "true"
@@ -158,7 +158,7 @@ resource "aws_route_table" "public" {
 }
 
 resource "aws_route_table_association" "public-1"{
-  subnet_id = "${aws_subnet.public-1.id}"
+  subnet_id = "${aws_subnet.public_1.id}"
   route_table_id = "${aws_route_table.public.id}"
 }
 
@@ -169,7 +169,7 @@ resource "aws_route_table_association" "public-1"{
 # We're NAT-ing the egress through a gateway having a public eip.
 
 
-resource "aws_subnet" "private-1" {
+resource "aws_subnet" "private_1" {
   vpc_id = "${aws_vpc.main.id}"
   cidr_block = "10.0.1.0/24"
   availability_zone = "${var.region}a"
@@ -187,13 +187,13 @@ resource "aws_eip" "nat_gateway" {
 
 resource "aws_nat_gateway" "main" {
   allocation_id = aws_eip.nat_gateway.id
-  subnet_id     = "${aws_subnet.public-1.id}"
+  subnet_id     = "${aws_subnet.public_1.id}"
   tags = {
     project = "${var.project}"
   }
 }
 
-resource "aws_route_table" "private-1" {
+resource "aws_route_table" "private_1" {
   vpc_id = "${aws_vpc.main.id}"
   route {
     cidr_block = "0.0.0.0/0"
@@ -205,9 +205,9 @@ resource "aws_route_table" "private-1" {
   }
 }
 
-resource "aws_route_table_association" "private-1"{
-  subnet_id = "${aws_subnet.private-1.id}"
-  route_table_id = "${aws_route_table.private-1.id}"
+resource "aws_route_table_association" "private_1"{
+  subnet_id = "${aws_subnet.private_1.id}"
+  route_table_id = "${aws_route_table.private_1.id}"
 }
 
 # EC2 Security Groups
@@ -328,7 +328,7 @@ resource "aws_emr_cluster" "accumulator" {
   applications = [ "Hadoop" ]
   service_role = "EMR_DefaultRole"
   ec2_attributes {
-    subnet_id                         = aws_subnet.private-1.id
+    subnet_id                         = aws_subnet.private_1.id
     additional_master_security_groups = "${aws_security_group.emr_master.id}"
     additional_slave_security_groups  = "${aws_security_group.emr_core.id}"
     instance_profile                  = aws_iam_instance_profile.emr_ec2.name
@@ -340,7 +340,7 @@ resource "aws_emr_cluster" "accumulator" {
   }
 
   core_instance_group {
-    instance_count = 2
+    instance_count = 1
     instance_type  = "m5a.xlarge"
     # TODO: set up autoscaling policy w/ spot instances.
   }
@@ -352,5 +352,69 @@ resource "aws_emr_cluster" "accumulator" {
   tags = {
     for-use-with-amazon-emr-managed-policies = true
     project = "mina"
+  }
+}
+
+
+# Sequencer EC2 Setup
+#####################
+
+data "aws_iam_policy_document" "sequencer_assume_role_policy" {
+  statement {
+    effect = "Allow"
+    principals {
+      type        = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
+    actions = [
+      "sts:AssumeRole",
+    ]
+  }
+}
+
+data "aws_iam_policy_document" "sequencer_role_policy" {
+  statement {
+    effect = "Allow"
+    actions = [
+      "elasticmapreduce:AddJobFlowSteps",
+      "elasticmapreduce:AddTags",
+      "elasticmapreduce:CancelStep",
+      "elasticmapreduce:Describe*",
+      "elasticmapreduce:Get*",
+      "elasticmapreduce:List*",
+      "elasticmapreduce:RunJobFlow",
+      "elasticmapreduce:TerminateJobFlows",
+    ]
+    resources = ["*"]
+  }
+}
+
+resource "aws_iam_role" "sequencer_role" {
+  name = "sequencer-role"
+  path = "/"
+  assume_role_policy = data.aws_iam_policy_document.sequencer_assume_role_policy.json
+  inline_policy {
+    name = "emr-access"
+    policy = data.aws_iam_policy_document.sequencer_role_policy.json
+  }
+}
+
+resource "aws_iam_instance_profile" "sequencer_emr_profile" {
+  name = "test_profile"
+  role = aws_iam_role.sequencer_role.name
+}
+
+resource "aws_instance" "Sequencer" {
+  ami = "ami-0d6ee9d5e1c985df6" # NixOS 23.05.426.afc48694f2a
+  subnet_id = aws_subnet.public_1.id
+  instance_type = "m5a.large"
+  user_data = file("./sequencer-nixos-config.nix")
+  iam_instance_profile = aws_iam_instance_profile.emr_ec2.name
+  vpc_security_group_ids = [ aws_security_group.sequencer.id ]
+  root_block_device {
+    volume_size = 15
+  }
+  tags = {
+    project = "${var.project}"
   }
 }
