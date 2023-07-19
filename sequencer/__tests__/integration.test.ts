bdb0aa6bd6fd2d0628bd86fb1c365647fc58ff76
ycryptx <yonatan.med@gmail.com>
2023-07-19 08:02:18 -0400

    Separate accumulator and sequencer tests (#18)
    * separate sequencer tests
    
    * separate steps tests & add ci

--- sequencer/__tests__/integration.test.ts
+++ sequencer/__tests__/integration.test.ts
@@ -7,12 +7,12 @@ describe('integration tests', () => {
   let mapReduce: MapReduceClient;
 
   beforeAll(() => {
-    process.env.MAPPER_FILE_PATH = '../../' + process.env.MAPPER_FILE_PATH;
-    process.env.REDUCER_FILE_PATH = '../../' + process.env.REDUCER_FILE_PATH;
+    process.env.MAPPER_FILE_PATH = '../../sequencer/__tests__/misc/mapper.js';
+    process.env.REDUCER_FILE_PATH = '../../sequencer/__tests__/misc/reducer.js';
     mapReduce = new MapReduceClient(Mode.LOCAL, 'ap-northeast-1');
   });
   it('1. demo-0: should sum numbers correctly', async () => {
-    const dataFilePath = path.join(__dirname, '../', 'data/run1.txt');
+    const dataFilePath = path.join(__dirname, 'misc/run.txt');
     const inputLocation = await mapReduce.upload(dataFilePath);
     expect(await mapReduce.process(inputLocation)).toEqual(`${2 * 8 + 8}`);
   });
