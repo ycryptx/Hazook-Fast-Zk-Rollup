20f1610e3a6ae0e1a4ea2d9c11286b8931489d1f
ycryptx <yonatan.med@gmail.com>
2023-07-21 10:48:58 +0200

    accumulator tests

--- accumulator/steps/steps/__tests__/main.test.ts
+++ accumulator/steps/steps/__tests__/main.test.ts
@@ -1,7 +1,17 @@
+import * as fs from 'fs';
 import * as path from 'path';
+import * as readline from 'readline';
 describe('test map reduce', () => {
   it('case 1', () => {
-    path.join(__dirname, 'misc/run.txt');
+    const rl = readline.createInterface({
+      input: fs.createReadStream(path.join(__dirname, 'misc/run.txt')),
+      crlfDelay: Infinity,
+    });
+    require('../src/mapper');
+    require('../src/reducer');
+    rl.on('line', (line) => {
+      console.log(line);
+    });
     expect(true).toBe(true);
   });
 });
