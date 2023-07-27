030370e0dc7af77eb0ec2409508245a3004da53c
ycryptx <yonatan.med@gmail.com>
2023-07-28 00:02:55 +0300

    add NUMA to Hadoop

--- accumulator/infra/emr_bootstrap_script.sh
+++ accumulator/infra/emr_bootstrap_script.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# install Node.js and Snarkyjs as a global package
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
 . ~/.nvm/nvm.sh
 nvm install 17
@@ -17,3 +18,18 @@ ls $NODE_PATH
 npm i -g snarkyjs
 
 ls $NODE_PATH
+
+# install and setup NUMA
+sudo yum -y install numactl
+echo 1 | sudo tee /proc/sys/kernel/numa_balancing
+
+echo "banned.users=mapred,bin,hdfs" >> /etc/hadoop/conf/container-executor.cfg
+rm -rf /var/log/hadoop-yarn/ 
+sudo chown -R yarn:hadoop /var/log/hadoop-yarn/
+sudo chmod 755 -R /var/log/hadoop-yarn/
+
+sudo chmod 6050 /etc/hadoop/conf/container-executor.cfg
+
+mkdir /mnt/yarn && sudo chmod 755 -R /mnt/yarn && sudo chown -R yarn:hadoop /mnt/yarn
+mkdir /mnt1/yarn && sudo chmod 755 -R /mnt1/yarn && sudo chown -R yarn:hadoop /mnt1/yarn
+mkdir /mnt2/yarn && sudo chmod 755 -R /mnt2/yarn && sudo chown -R yarn:hadoop /mnt2/yarn
