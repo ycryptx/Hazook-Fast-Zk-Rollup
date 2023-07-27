6d7e3e9f7192b04c7186910a8b164f904eb12e1b
ycryptx <yonatan.med@gmail.com>
2023-07-28 02:04:09 +0300

    test out different config

--- sequencer/src/map-reduce/client/client.ts
+++ sequencer/src/map-reduce/client/client.ts
@@ -139,15 +139,16 @@ export class MapReduceClient {
         {
           Classification: 'mapred-site',
           Properties: {
-            'mapreduce.map.memory.mb': '10240', // Set the memory for each mapper task to 5GB (in MB)
-            'mapreduce.map.java.opts': '-Xmx5120m',
+            'mapreduce.map.memory.mb': '10240', // Set the memory for each mapper task to 10GB (in MB)
+            'mapreduce.task.timeout': '0',
+            'mapreduce.task.profile': 'true',
           },
         },
         {
           Classification: 'yarn-site',
           Properties: {
-            'yarn.nodemanager.resource.memory-mb': '12288', // Set the total memory available to YARN on the master node to 28GB (in MB)
-            'yarn.scheduler.maximum-allocation-mb': '12288', // Set the maximum memory allocation for a single container to 28GB (in MB)
+            'yarn.nodemanager.resource.memory-mb': '5120', // Set the total memory available to YARN on the master node to 5GB (in MB)
+            'yarn.scheduler.maximum-allocation-mb': '5120', // Set the maximum memory allocation for a single container to 5GB (in MB)
           },
         },
         // Add any other YARN or Hadoop configurations you require
