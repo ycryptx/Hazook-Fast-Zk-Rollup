2dc363a1b9c30c62c163fb5bc906e8b3b6fbf8c4
Félix Baylac Jacqué <felix@alternativebit.fr>
2023-08-28 13:50:39 +0200

    Infra: create dev sg + keypair
    These ressources are interactively used from the AWS CLI.

--- accumulator/infra/main.tf
+++ accumulator/infra/main.tf
@@ -283,6 +283,22 @@ resource "aws_security_group" "sequencer" {
   }
 }
 
+resource "aws_security_group" "emr_dev" {
+  vpc_id = "${aws_vpc.main.id}"
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = [ aws_subnet.private_1.cidr_block ]
+  }
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = -1
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 resource "aws_security_group" "emr_master" {
   vpc_id = aws_vpc.main.id
   egress {
@@ -486,6 +502,11 @@ resource "aws_eip" "sequencer-eip" {
 resource "aws_key_pair" "ycryptx" {
   key_name = "ycryptx"
   public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCz7QlDRx7Vvra3XCfB4bWwFSxEgw81DHgeNrFTR5dxT/J29MfZhW+rjJXR4mVAvUGEBlNsGJ6EwBt65FqWxuWTGARoW2jBVMxqwqxldYLKHWcWTv8IdaYAQniKwfOX/3NaaQEw93HwHbb8aYjbBudR/UtwOgT0vDpuxUzPwIDRxea3Za64qV0H7s6PnfbC5DcC9fOX72fiGXuwMaZAUN8dIgI9mZcEn3yaWfwqYQ+Qcx6pDEWG73YLXJfoZ7UtSp+GF6lgOcTc7pw+NIoUcU/Pq+I0d7ECIEaRXv97U2R8lbgBRkR7NIBjxqSKHb3m5wfDvLQGrrn2Mg7zmGa8buyfeNaBfolEfa+c8R2fS8smvd7El3K/ogMeRJ3j5actRIP74UKqrgQd6nTJDkxD4F09bDHcke+PLlLkyURnatcRGH3J56sVTXRM5mRGuoFufBz8s6K+jS2Fmxirf97fJ61gq/M7w4LEDDX2gncrNeX+QmqGeWXV5wBFkvS2lxYGl88="
+}
+
+resource "aws_key_pair" "sequencer" {
+  key_name = "sequencer"
+  public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJ6tWZZkHYJJtD7G+hiOc8ICbNrDngrLtE/jst67wERX"
 }
 
 resource "aws_instance" "sequencer" {
